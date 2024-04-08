2f92b832295d3ec18548c3650b90f9f90eaad949
Eitan Turok <eitan.turok@databricks.com>
2024-04-08 16:44:51 +0000

    remove extra \t, \n from prompts

--- berkeley-function-call-leaderboard/model_handler/constant.py
+++ berkeley-function-call-leaderboard/model_handler/constant.py
@@ -1,15 +1,11 @@
-SYSTEM_PROMPT_FOR_CHAT_MODEL = """"
-    You are an expert in composing functions. You are given a question and a set of possible functions.
-    Based on the question, you will need to make one or more function/tool calls to achieve the purpose.
-    If none of the function can be used, point it out. If the given question lacks the parameters required by the function,
-    also point it out. You should only return the function call in tools call sections.
-    """
+SYSTEM_PROMPT_FOR_CHAT_MODEL = """"You are an expert in composing functions. You are given a question and a set of possible functions.
+Based on the question, you will need to make one or more function/tool calls to achieve the purpose.
+If none of the function can be used, point it out. If the given question lacks the parameters required by the function,
+also point it out. You should only return the function call in tools call sections."""
 
-USER_PROMPT_FOR_CHAT_MODEL = """
-    Questions:{user_prompt}\nHere is a list of functions in JSON format that you can invoke:\n{functions}.
-    Should you decide to return the function call(s),Put it in the format of [func1(params_name=params_value, params_name2=params_value2...), func2(params)]\n
-    NO other text MUST be included.
-"""
+USER_PROMPT_FOR_CHAT_MODEL = """Questions:{user_prompt}\nHere is a list of functions in JSON format that you can invoke:\n{functions}.Should you decide to return the function call(s),Put it in the format of [func1(params_name=params_value, params_name2=params_value2...), func2(params)]\n
+NO other text MUST be included."""
+
 GORILLA_TO_OPENAPI = {
     "integer": "integer",
     "number": "number",
@@ -79,6 +75,10 @@ MODEL_ID_DICT = {
     "gorilla-openfunctions-v0": "gorilla",
     "functionary-small-v2.2": "mistral",
     "functionary-medium-v2.2": "mistral",
+}
+
+MODEL_ID_TO_NAME = {
+    "EleutherAI/pythia-70m": "pythia-70m",
 }
 
 JAVA_TYPE_CONVERSION = {
