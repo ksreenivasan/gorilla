d486b8a8daaf71ad91f6895a977fbadba571a65d
Eitan Turok <eitan.turok@databricks.com>
2024-04-08 17:16:55 +0000

    tokenize with templates; decode_execute

--- berkeley-function-call-leaderboard/model_handler/accelerate_handler.py
+++ berkeley-function-call-leaderboard/model_handler/accelerate_handler.py
@@ -14,51 +14,94 @@ from model_handler.utils import (
     ast_parse,
     augment_prompt_by_languge,
     language_specific_pre_processing,
-    construct_tool_use_system_prompt,
     _function_calls_valid_format_and_invoke_extraction,
     _convert_value,
 )
 from model_handler.constant import (
     SYSTEM_PROMPT_FOR_CHAT_MODEL,
     USER_PROMPT_FOR_CHAT_MODEL,
+    MODEL_ID_TO_NAME,
 )
 
 class AccelerateHandler(BaseHandler):
-    def __init__(self, model_name, checkpoint_dir=".checkpoint/", temperature=0.7, top_p=1, max_tokens=1000) -> None:
-        super().__init__(model_name, temperature, top_p, max_tokens)
-        self.model_style = ModelStyle.Accelerate
-        self.checkpoint_dir = checkpoint_dir
-        self.model = self._init_model()
-        self.tokenizer = self._init_tokenizer()
+
+    def get_model_name(self, model_id):
+        if model_id in MODEL_ID_TO_NAME:
+            return MODEL_ID_TO_NAME[model_id]
+        # Assume this is a HF model where the model name is after the last "/"
+        return model_id.split("/")[-1]
 
     def _init_model(self):
         """From https://huggingface.co/docs/accelerate/usage_guides/big_modeling#complete-example"""
-        weights_location = hf_hub_download(self.model_name, "pytorch_model.bin", token=True)
+        weights_location = hf_hub_download(self.model_id, "pytorch_model.bin", token=True)
         with init_empty_weights():
-            model = AutoModelForCausalLM.from_pretrained(self.model_name, trust_remote_code=True, use_auth_token=True)
+            model = AutoModelForCausalLM.from_pretrained(self.model_id, trust_remote_code=True, use_auth_token=True)
         model = load_checkpoint_and_dispatch(model, checkpoint=weights_location, device_map="auto")
         return model
 
     def _init_tokenizer(self):
         tokenizer = AutoTokenizer.from_pretrained(
-                self.model_name,
+                self.model_id,
                 trust_remote_code=True,
                 use_auth_token=True,
                 truncation_side="left",
-                padding_side="right",
+                # padding_side="right",
             )
         return tokenizer
 
-    def _format_prompt_func(self, prompt, function):
-        SYSTEM_PROMPT = """You are an helpful assistant who has access to the following functions to help the user, you can use the functions if needed-"""
-        functions = ""
-        if isinstance(function, list):
-            for idx, func in enumerate(function):
-                functions += "\n" + str(func)
-        else:
-            functions += "\n" + str(function)
-        return f"SYSTEM: {SYSTEM_PROMPT}\n{functions}\nUSER: {prompt}\nASSISTANT: "
+    def __init__(self, model_id, temperature=0.7, top_p=1, max_tokens=1000, do_inference=False) -> None:
+        self.model_id = model_id
+        self.do_inference = do_inference
+        model_name = self.get_model_name(model_id)
+        super().__init__(model_name, temperature, top_p, max_tokens)
+        self.model_style = ModelStyle.Accelerate
+        if self.do_inference:
+            self.model = self._init_model()
+            self.tokenizer = self._init_tokenizer()
 
+    def _format_prompt_func(self, prompt, function):
+        user_prompt = USER_PROMPT_FOR_CHAT_MODEL.format(
+            user_prompt=prompt, functions=str(function)
+            )
+        system_prompt = SYSTEM_PROMPT_FOR_CHAT_MODEL
+
+        #return f"SYSTEM: {system_prompt}\nUSER: {user_prompt}\nASSISTANT: "
+        return user_prompt, system_prompt
+
+    def tokenizer_encode(self, user_prompt, system_prompt, tokenize=True, template=None):
+        """Tokenize the text with apply_tool_use_template, apply_chat_template, or with
+        no template.
+
+        If template is `None`, then tokenize first with tool use template, then chat template, and
+        then no template (in this order), if the tokenizer has these features. You can override
+        this by setting template to `tool_use` or `chat`.
+        """
+
+        if template == "tool_use" or (hasattr(self.tokenizer, "apply_tool_use_template") and template is None):
+            messages = [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+                ]
+            tools = None
+            inputs = self.tokenizer.apply_chat_template(
+                messages, tools=tools, tokenize=tokenize, add_generation_prompt=True,
+                return_tensors="pt", return_dict=True)
+        elif template == "chat" or (hasattr(self.tokenizer, "apply_chat_template") and template is None):
+            messages = [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+                ]
+            inputs = self.tokenizer.apply_chat_template(
+                messages, tokenize=tokenize, add_generation_prompt=True, return_tensors="pt", return_dict=True)
+        else:
+            messages = [f"SYSTEM: {system_prompt}\nUSER: {user_prompt}\nASSISTANT: "]
+            inputs = messages
+            if tokenize:
+                inputs = self.tokenizer(messages, return_tensors="pt")
+
+        if tokenize:
+            inputs = inputs.to("cuda")
+        return inputs
 
     def inference(self, prompt, functions, test_category):
         start = time.time()
@@ -66,29 +109,29 @@ class AccelerateHandler(BaseHandler):
         # Format the prompt with the functions the model has access to
         functions = language_specific_pre_processing(functions, test_category, False)
         prompt = augment_prompt_by_languge(prompt, test_category)
-        prompt = self._format_prompt_func(prompt, functions)
+        user_prompt, system_prompt = self._format_prompt_func(prompt, functions)
 
-        # Tokenize, generate, decode
-        inputs = self.tokenizer(prompt, return_tensors="pt").to("cuda")
-        outputs = self.model.generate(**inputs, max_new_tokens=200)
-        result = self.tokenizer.decode(outputs[0])
+        # Tokenize
+        input_tokens = self.tokenizer_encode(user_prompt, system_prompt)
+
+        # Generate
+        outputs = self.model.generate(**input_tokens, pad_token_id=self.tokenizer.eos_token_id, max_new_tokens=200)
+
+        # Decode
+        output_texts = self.tokenizer.decode(outputs[0])
+
+        # Process output
+        input_texts = self.tokenizer_encode(user_prompt, system_prompt, tokenize=False)
+        result = output_texts[len(input_texts):]
 
         # Record info
         latency = time.time() - start
         metadata = {
-            "input_tokens": prompt,
-            "output_tokens": result[len(prompt):],
+            "input_tokens": input_texts,
+            "output_tokens": result,
             "latency": latency
             }
         return result, metadata
 
-    def write(self, result, file_to_open):
-        model_name = self.model_name
-        model_name_path =  model_name.replace("/", "_")
-        if not os.path.exists("./result"):
-            os.mkdir("./result")
-        out_dir = os.path.join("./result", model_name_path)
-        if not os.path.exists(out_dir):
-            os.mkdir(out_dir)
-        with open(os.path.join(out_dir, file_to_open), "a+") as f:
-            f.write(json.dumps(result) + "\n")
+    def decode_execute(self, result):
+        return result
