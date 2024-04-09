e860d319084a5245e0126ee97743b41a7175be1e
Eitan Turok <eitan.turok@databricks.com>
2024-04-09 05:36:47 +0000

    make cuda device=7

--- berkeley-function-call-leaderboard/model_handler/accelerate_handler.py
+++ berkeley-function-call-leaderboard/model_handler/accelerate_handler.py
@@ -96,7 +96,7 @@ class AccelerateHandler(BaseHandler):
                 inputs = self.tokenizer(messages, return_tensors="pt")
 
         if tokenize:
-            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+            device = 'cuda:7' if torch.cuda.is_available() else 'cpu'
             inputs = inputs.to(device)
         return inputs
 
