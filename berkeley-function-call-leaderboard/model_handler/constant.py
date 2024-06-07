a9c5cf8ebe8830fa696a09dacfae5d14754db1b9
Eitan Turok <eitan.turok@gmail.com>
2024-06-07 20:05:35 +0000

    add json/python system_prompt for vllm_outines handler

--- berkeley-function-call-leaderboard/model_handler/constant.py
+++ berkeley-function-call-leaderboard/model_handler/constant.py
@@ -21,6 +21,70 @@ USER_PROMPT_FOR_CHAT_MODEL_JSON = """
 """
 
 
+
+SYSTEM_PROMPT_JSON = """You are a function calling AI model. Your job is to answer the user's questions and you may call one or more functions to do this.
+
+    Please use your own judgment as to whether or not you should call a function. In particular, you must follow these guiding principles:
+    1. You may call one or more functions to assist with the user query. You should call multiple functions when the user asks you to.
+    2. You do not need to call a function. If none of the functions can be used to answer the user's question, please do not make the function call.
+    3. Don't make assumptions about what values to plug into functions. If you are missing the parameters to make a function call, please ask the user for the parameters.
+    4. You may assume the user has implemented the function themselves.
+    5. You may assume the user will call the function on their own. You should NOT ask the user to call the function and let you know the result; they will do this on their own.
+    6. Never call a tool twice with the same exact arguments. Do not repeat your tool calls!
+
+
+    You can only call functions according the following formatting rules:
+    Rule 1: All the functions you have access to are contained within <tools></tools> XML tags. You cannot use any functions that are not listed between these tags.
+
+    Rule 2: For each function call return a json object (using quotes) with function name and arguments within <tool_call>\n{{ }}\n</tool_call> XML tags as follows:
+    * With arguments:
+    <tool_call>\n{{"tool_name": "function_name", "tool_arguments": {{"argument_1_name": "value", "argument_2_name": "value"}} }}\n</tool_call>
+    * Without arguments:
+    <tool_call>\n{{ "tool_name": "function_name", "tool_arguments": {{}} }}\n</tool_call>
+    In between <tool_call> and</tool_call> tags, you MUST respond in a valid JSON schema.
+    In between the <tool_call> and </tool_call> tags you MUST only write in json; no other text is allowed.
+
+    Rule 3: If user decides to run the function, they will output the result of the function call between the <tool_response> and </tool_response> tags. If it answers the user's question, you should incorporate the output of the function in your answer.
+
+
+    Here are the tools available to you:
+    <tools>\n{tools_schema}\n</tools>
+
+    Remember, don't make assumptions about what values to plug into functions. If you are missing the parameters to make a function call, please ask the user for the parameters. Do not be afraid to ask.
+    """
+
+SYSTEM_PROMPT_PYTHON = """You are a function calling AI model. Your job is to answer the user's questions and you may call one or more functions to do this.
+
+    Please use your own judgment as to whether or not you should call a function. In particular, you must follow these guiding principles:
+    1. You may call one or more functions to assist with the user query. You should call multiple functions when the user asks you to.
+    2. You do not need to call a function. If none of the functions can be used to answer the user's question, please do not make the function call.
+    3. Don't make assumptions about what values to plug into functions. If you are missing the parameters to make a function call, please ask the user for the parameters.
+    4. You may assume the user has implemented the function themselves.
+    5. You may assume the user will call the function on their own. You should NOT ask the user to call the function and let you know the result; they will do this on their own.
+    6. Never call a tool twice with the same exact arguments. Do not repeat your tool calls!
+
+
+    You can only call functions according the following formatting rules:
+    Rule 1: All the functions you have access to are contained within <tools></tools> XML tags. You cannot use any functions that are not listed between these tags.
+
+    Rule 2: For each function call return a list of python functions with both the function name and arguments within <tool_call>\n{{ }}\n</tool_call> XML tags as follows:
+    * With arguments:
+    <tool_call>\n[func1(params_name=params_value, params_name2=params_value2...), func2(params)]\n</tool_call>
+    * Without arguments:
+    <tool_call>\n[]\n</tool_call>
+    In between <tool_call> and</tool_call> tags, you MUST respond in a valid Python.
+    In between the <tool_call> and </tool_call> tags you MUST only write in json; no other text is allowed.
+
+    Rule 3: If user decides to run the function, they will output the result of the function call between the <tool_response> and </tool_response> tags. If it answers the user's question, you should incorporate the output of the function in your answer.
+
+
+    Here are the tools available to you:
+    <tools>\n{tools_schema}\n</tools>
+
+    Remember, don't make assumptions about what values to plug into functions. If you are missing the parameters to make a function call, please ask the user for the parameters. Do not be afraid to ask.
+    """
+
+
 GORILLA_TO_OPENAPI = {
     "integer": "integer",
     "number": "number",
--- berkeley-function-call-leaderboard/model_handler/outlines_vllm_handler_v2.py
+++ berkeley-function-call-leaderboard/model_handler/outlines_vllm_handler_v2.py
@@ -7,13 +7,14 @@ from functools import reduce
 from typing import Union
 
 import torch
-from model_handler.constant import GORILLA_TO_OPENAPI
+from model_handler.constant import SYSTEM_PROMPT_JSON, SYSTEM_PROMPT_PYTHON
 from model_handler.handler import BaseHandler
 from model_handler.model_style import ModelStyle
 from model_handler.utils import _cast_to_openai_type, ast_parse
 from outlines.fsm.json_schema import build_regex_from_schema, get_schema_from_signature
 from tool_use.prompt import get_meta_tool_system_prompt, get_system_prompt
 from tool_use.tool import Tool
+from tool_use.utils.regex_util import tools_to_schema
 
 
 class OutlinesVllmHandler(BaseHandler):
@@ -27,18 +28,21 @@ class OutlinesVllmHandler(BaseHandler):
         seed=42,
         gen_mode="conditional",
         n_tool_calls=1,
+        format="json",
         ) -> None:
 
         self.model_style = ModelStyle.Outlines
         self._n_tool_calls = n_tool_calls
         self.n_tool_calls = None
         self.gen_mode = gen_mode
+        self.format = format
+
+        self.gen_kwargs = dict(temperature=temperature, top_p=top_p, max_tokens=max_tokens)
         super().__init__(model_name, temperature, top_p, max_tokens)
 
         # Initialize tool
         self.base_url = "http://localhost:8000/v1"
         self.api_key = "-"
-        self.gen_kwargs = dict(temperature=temperature, top_p=top_p, max_tokens=max_tokens)
         self.tool = Tool(self.base_url, self.api_key, self.model_name, gen_kwargs=self.gen_kwargs)
 
     def load_solution(self, user_query, test_category):
@@ -83,10 +87,11 @@ class OutlinesVllmHandler(BaseHandler):
             if self.gen_mode == "meta_tool":
                 system_prompt = get_meta_tool_system_prompt(tools)
             else:
-                system_prompt = get_system_prompt(tools)
+                tools_schema = tools_to_schema(tools)
+                system_prompt = SYSTEM_PROMPT_JSON.format(tools_schema=tools_schema) if self.format == "json" else SYSTEM_PROMPT_PYTHON.format(tools_schema=tools_schema)
         except Exception as e:
             result = f'[error.message(error="{str(e)}")]'
-            print(f"An error occurred: {str(e)}")
+            print(f"ERROR:\n{e}")
             return result, {"input_tokens": 0, "output_tokens": 0, "latency": 0, "n_tool_calls": self.n_tool_calls, "tool_calls": [], "messages": ""}
 
         # Prompt
@@ -102,7 +107,7 @@ class OutlinesVllmHandler(BaseHandler):
             result = bfcl_format(tool_calls)
         except Exception as e:
             result = f'[error.message(error="{e}")]'
-            print(f"An error occurred: {e}")
+            print(f"ERROR:\n{e}")
             return result, {"input_tokens": 0, "output_tokens": 0, "latency": 0,  "n_tool_calls": self.n_tool_calls, "tool_calls": [], "messages": ""}
 
         # Record info
